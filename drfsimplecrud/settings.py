5a5221b321e7fb3ba589d3670156648f96dcc1d3
draconreyes <draconreyes@gmail.com>
2025-05-12 17:25:10 -0500

    change settings.py

--- drfsimplecrud/settings.py
+++ drfsimplecrud/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'projects',
+    'projects.apps.ProjectsConfig',
     'rest_framework'
 ]
 
